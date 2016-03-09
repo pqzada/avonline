da502f740a8dbead56d8be6d20fe8ccad6102bc4
Pablo Quezada <pquezadar@neonline.cl>
2016-03-08 22:47:41 -0300

    Primer test de banners

--- modules/frontpage.php
+++ modules/frontpage.php
@@ -8,6 +8,18 @@ $ofertas = Oferta::findAllFrontPage();
 
 	<br>
 
+	<article class="col-xs-12 col-sm-4 col-md-3">
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<!-- @Pagina Desktop - Left -->
+		<ins class="adsbygoogle"
+		     style="display:inline-block;width:300px;height:250px"
+		     data-ad-client="ca-pub-3186329023014409"
+		     data-ad-slot="9227416175"></ins>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+	</article>
+
 	<? foreach($ofertas as $oferta) : ?>
 
 		<article class="col-xs-12 col-sm-4 col-md-3">
