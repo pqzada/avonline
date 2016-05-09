42b374ba200cf67d2fb780f3a61223c7db4add46
Pablo Quezada <pquezadar@neonline.cl>
2016-05-08 22:40:56 -0300

    Se modifica el listado de productos

--- modules/oferta.php
+++ modules/oferta.php
@@ -37,101 +37,116 @@ if(date('Y-m-d') > $oferta['fecha_fin']) {
 <title><?=$oferta["titulo"]?></title>
 
 <div class="oferta container">
-	<div class="page-header">
-		<h1><?=$oferta["titulo"]?></h1>
+
+	<div class="page-header row">
+		<div class="col-xs-12 col-sm-9">
+			<h1><?=$oferta["titulo"]?></h1>
+		</div>
+		<div class="col-xs-12 col-sm-3">
+			<? if($oferta["id_estado"] == "PUBLICADA"): ?>
+				<a href="<?=$oferta["url_externa"]?>" class="btn btn-danger btn-lg pull-right" rel="no-follow" target="_blank">Visitar oferta</a>
+			<? endif; ?>
+		</div>
 	</div>
 
-	<div class="col-sm-4">
-		<img src="<?=$oferta["imagen"]?>" style="width:100%;">
-		
-		<br><br>
+	<div class="row">
 
-		<? if($oferta['id_estado'] == 'PUBLICADA')  : ?>
-			<article class=" banner">
-				<div class="thumbnail">
-					<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-					<!-- @Pagina Desktop - Left AND Mobile-->
-					<ins class="adsbygoogle"
-					     style="display:inline-block;width:300px;height:250px"
-					     data-ad-client="ca-pub-3186329023014409"
-					     data-ad-slot="9227416175"></ins>
-					<script>
-					(adsbygoogle = window.adsbygoogle || []).push({});
-					</script>
-				</div>
-			</article>
-		<? endif; ?>
-	</div>
+		<div class="col-sm-4">
 
-	<div class="col-sm-8">
-		<p><?=$oferta["descripcion"]?></p>
+			<img src="<?=$oferta["imagen"]?>" style="width:100%;">
+			
+			<br><br>
 
-		<div class="meta">
-			<label>Categorias</label>
-			<? foreach($oferta["categorias"] as $categoria): ?>
-				<h2><a href="/<?=$categoria["id"]?>"><?=$categoria["nombre"]?></a></h2>
-			<? endforeach; ?>
-
-			<br>
-
-			<label>Tags</label>
-			<? foreach($oferta["tags"] as $tag): ?>
-				<h3><a href="/tag/<?=$tag["id"]?>"><?=$tag["nombre"]?></a></h3>
-			<? endforeach; ?>
+			<? if($oferta['id_estado'] == 'PUBLICADA')  : ?>
+				<article class=" banner">
+					<div class="thumbnail">
+						<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+						<!-- @Pagina Desktop - Left AND Mobile-->
+						<ins class="adsbygoogle"
+						     style="display:inline-block;width:300px;height:250px"
+						     data-ad-client="ca-pub-3186329023014409"
+						     data-ad-slot="9227416175"></ins>
+						<script>
+						(adsbygoogle = window.adsbygoogle || []).push({});
+						</script>
+					</div>
+				</article>
+			<? endif; ?>
 		</div>
 
-		<hr>
+		<div class="col-sm-8">
 
-		<? if($oferta["id_estado"] == "PUBLICADA"): ?>
-			<a href="<?=$oferta["url_externa"]?>" class="btn btn-danger btn-lg pull-right" rel="no-follow" target="_blank">Visitar oferta</a>
-		<? endif; ?>
+			<p><?=$oferta["descripcion"]?>. <br>Entre los productos de esta oferta encontrarás: </p>		
 
-		<? if(count($productos) > 0): ?>
-			<h2>Algunos productos que encontrarás...</h2>
+			<? if(count($productos) > 0): ?>
 
-			<? foreach($productos as $producto): ?>
-				<div class="producto row">
-					<div class="producto-imagen col-xs-2 thumbnail">	
-						<img src="<?=$producto["imagen"]?>">
+				<? foreach($productos as $producto): ?>
+					<div class="producto">
+
+						<div class="media">
+							<div class="media-left media-middle">
+								<img class="media-object" src="<?=$producto["imagen"]?>" alt="<?=$producto["nombre"]?>">
+							</div>
+							<div class="media-body">
+								<h4 class="media-heading"><?=$producto["nombre"]?></h4>
+								<?=$producto["descripcion"]?>
+							</div>
+						</div>						
+
 					</div>
-					<div class="producto-info col-xs-10">
-						<h3><?=$producto["nombre"]?></h3>
-						<p><?=$producto["descripcion"]?></p>
-					</div> 
+				<? endforeach; ?>
+
+			<? endif; ?>
+
+			<? if($oferta["id_estado"] == "PUBLICADA"): ?>
+				<div class="revisa row">
+					<div class="col-xs-12">
+						<a href="<?=$oferta["url_externa"]?>" class="btn btn-primary pull-right" rel="no-follow" target="_blank">Revisa estos y más productos...</a>
+					</div>
 				</div>
-			<? endforeach; ?>
+			<? endif; ?>
 
-		<? endif; ?>
-
-		<? if($oferta["id_estado"] == "PUBLICADA"): ?>
-			<div class="row">
-				<a href="<?=$oferta["url_externa"]?>" class="btn btn-primary btn-lg pull-right" rel="no-follow" target="_blank">Revisa estos y más productos...</a>
+			<div class="meta row">
+				<div class="col-xs-12 col-sm-6">
+					<label>Categorias</label>
+					<? foreach($oferta["categorias"] as $categoria): ?>
+						<h2><a href="/<?=$categoria["id"]?>"><?=$categoria["nombre"]?></a></h2>
+					<? endforeach; ?>
+				</div>
+				<div class="col-xs-12 col-sm-6">
+					<label>Tags</label>
+					<? foreach($oferta["tags"] as $tag): ?>
+						<h3><a href="/tag/<?=$tag["id"]?>"><?=$tag["nombre"]?></a></h3>
+					<? endforeach; ?>
+				</div>
 			</div>
-		<? endif; ?>
 
-		<? if($oferta["id_estado"] == "DESACTIVADA"): ?>
-			<h4>Oferta no vigente<br><small>Revisa las siguientes ofertas similares</small></h4>
-		<? else: ?>
-			<h4>Revisa las siguientes ofertas similares</small></h4>
-		<? endif; ?>
-	
-		<div class="row similares">
-			<? foreach($similares as $s) : ?>
-				<article class="col-xs-12">
-					<div class="media">
-						<div class="media-left">
-							<a href="<?=$s["url_interna"]?>">
-								<img class="media-object" src="<?=$s["imagen"]?>" alt="<?=$s["titulo"]?>">
-							</a>
-						</div>
-						<div class="media-body">
-							<h4 class="media-heading"><a href="<?=$s["url_interna"]?>"><?=$s["titulo"]?></a></h4>
-							<?=$s["descripcion"]?>
-						</div>
-					</div>		
-				</article>
-			<? endforeach; ?>
-		</div>	
+			<br><br>
 
+			<? if($oferta["id_estado"] == "DESACTIVADA"): ?>
+				<h4>Oferta no vigente<br><small>Revisa las siguientes ofertas similares</small></h4>
+			<? else: ?>
+				<h4>Revisa las siguientes ofertas similares</small></h4>
+			<? endif; ?>
+		
+			<div class="row similares">
+				<? foreach($similares as $s) : ?>
+					<article class="col-xs-12">
+						<div class="media">
+							<div class="media-left">
+								<a href="<?=$s["url_interna"]?>">
+									<img class="media-object" src="<?=$s["imagen"]?>" alt="<?=$s["titulo"]?>">
+								</a>
+							</div>
+							<div class="media-body">
+								<h4 class="media-heading"><a href="<?=$s["url_interna"]?>"><?=$s["titulo"]?></a></h4>
+								<?=$s["descripcion"]?>
+							</div>
+						</div>		
+					</article>
+				<? endforeach; ?>
+			</div>	
+
+		</div>
 	</div>
 </div>
