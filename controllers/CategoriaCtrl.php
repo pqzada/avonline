6e82a131d8f039a757e400feac36bad733f390de
Pablo Quezada <pquezadar@neonline.cl>
2016-06-05 21:39:00 -0300

    Se realizan ajusten en categoria

--- controllers/CategoriaCtrl.php
+++ controllers/CategoriaCtrl.php
@@ -2,15 +2,15 @@
 $ofertas = Oferta::findAllForCategoria($_GET["categoria"]);
 $categoria = Categoria::findById($_GET["categoria"]);
 
-// $meta = array(
-// 	'title' => 'Descuentos en productos de ' . $categoria["titulo"],
-// 	'description' => 'Descuentos en ' . $categoria["titulo"] . ' en tiendas de internet.',
-// 	'facebook' => array(
-// 		'url' => 'http://www.avispateonline.cl/' . $categoria["id"],
-// 		'type' => 'website',
-// 		'title' => 'Descuentos en productos de ' . $categoria["titulo"] . ' en Avispate Online!', 
-// 		'description' => 'Encuentra en un sólo lugar las mejores ofertas de internet para ' . $categoria["titulo"],
-// 		'image' => Imagen::getImageData('http://www.avispateonline.cl/assets/images/Logo_Avispate_Grande_Fondo.png')
-// 	)
-// );
+$meta = array(
+	'title' => 'Descuentos en productos de ' . $categoria["titulo"],
+	'description' => 'Descuentos en ' . $categoria["titulo"] . ' en tiendas de internet.',
+	'facebook' => array(
+		'url' => 'http://www.avispateonline.cl/' . $categoria["id"],
+		'type' => 'website',
+		'title' => 'Descuentos en productos de ' . $categoria["titulo"] . ' en Avispate Online!', 
+		'description' => 'Encuentra en un sólo lugar las mejores ofertas de internet para ' . $categoria["titulo"],
+		'image' => Imagen::getImageData('http://www.avispateonline.cl/assets/images/Logo_Avispate_Grande_Fondo.png')
+	)
+);
 ?>
